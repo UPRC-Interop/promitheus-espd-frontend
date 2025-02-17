c5f5bde58265b033996033848d042e725d27be66
charis-ant <harisantoniadi@gmail.com>
2025-02-17 18:00:14 +0200

    Updated ojs validation regex #51

--- src/app/validation/number-ojs/number-ojs-validation.ts
+++ src/app/validation/number-ojs/number-ojs-validation.ts
@@ -14,23 +14,27 @@
 /// limitations under the License.
 ///
 
-import {AbstractControl} from "@angular/forms";
-import {StringHelperService} from "../../services/string-helper.service";
+import {AbstractControl} from '@angular/forms';
+import {StringHelperService} from '../../services/string-helper.service';
 
-const numberOjsRegExp: RegExp = new RegExp("(0000/S 000-000000)|^(20|19)\\d{2}/S (((00)?[1-9])|([0]?[1-9][0-9])|(1[0-9][0-9])|(2[0-5][0-9]))-\\d{6}$");
+const numberOjsRegExp: RegExp = new RegExp(
+  '^(0000/S 000-000000|((19|20)\\d{2}/S \\d{3}-\\d{6}))$'
+);
 
 export function NumberOjsValidation(control: AbstractControl): { [key: string]: boolean } | null {
 
-  let numberOjs = control.value;
+  const numberOjs = control.value;
 
   if (StringHelperService.isBlank(numberOjs)) {
     return null;
   }
+
   if (!numberOjsRegExp.test(numberOjs)) {
     return {
       'numberOjs': true
-    }
+    };
   }
+
   return null;
 
 }
